77968601ccf762563771edda49c688e4f7cd2163
bzx <1090943977@qq.com>
2023-04-09 11:30:07 +0800

    four

--- homework_1/app/src/test/java/cn/edu/jnu/x2020101340/BowlingGameUnitTest1.java
+++ homework_1/app/src/test/java/cn/edu/jnu/x2020101340/BowlingGameUnitTest1.java
@@ -27,10 +27,23 @@ public class BowlingGameUnitTest1 {
         assertEquals(40,game1.score());
     }
 
+    @Test
+    public void testASpare()
+    {
+        rollASpare1();
+        game1.roll(7);
+        repeatedRoll1(0,17);
+        assertEquals(24,game1.score());
+    }
     private void repeatedRoll1(int pin, int times) {
         for(int i = 0; i < times; i++) {
             game1.roll (pin);
         }
     }
+    private void rollASpare1() {
+        game1.roll(3);
+        game1.roll(7);
+    }
+
 
 }
